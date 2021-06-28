1047ae5b0f5c8c86fe3e7232825ad60b10812e16
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-29 00:20:21 +0600

    all page design & routing setup

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
-import Setting from "./pages/settings/Setting";
+import Settings from "./pages/settings/Setting";
 import Write from "./pages/write/Write";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 function App() {
+
+  const currentUser = false;
+
   return (
-    <>
+    <Router>
       <TopBar/>
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write/> */}
-      {/* <Setting/> */}
-      {/* <Login/> */}
-      <Register/>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">
+          {currentUser ? <Home /> : <Login />}
+        </Route>
+        <Route path="/write">
+         {currentUser ? <Write /> : <Login />}
+        </Route>
+        <Route path="/settings">
+         {currentUser ? <Settings /> : <Login />}
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
 import "./topbar.css";
 
+
 export default function TopBar() {
+ 
+    const user = false;
+
     return (
         <div className="top">
             <div className="topLeft">
@@ -11,15 +16,44 @@ export default function TopBar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">
+                    <Link className="link" to="/">
+                        HOME
+                    </Link>
+                    </li>
                     <li className="topListItem">ABOUT</li>
                     <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                    <Link className="link" to="/write">
+                        WRITE
+                    </Link>
+                    </li>
+                    {user && <li className="topListItem">LOGOUT</li>}
                 </ul>
             </div>
             <div className="topRight">
-                <img className="topImg" src="https://res.cloudinary.com/demo/image/twitter/1330457336.jpg"  alt=""/>
+            {user ? (
+                <Link className="link" to="/settings">
+                    <img
+                    className="topImg"
+                    src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                    alt=""
+                    />
+                </Link>
+                ) : (
+                <ul className="topList">
+                    <li className="topListItem">
+                    <Link className="link" to="/login">
+                        LOGIN
+                    </Link>
+                    </li>
+                    <li className="topListItem">
+                    <Link className="link" to="/register">
+                        REGISTER
+                    </Link>
+                    </li>
+                </ul>
+                )}
                 <i className="topSearchIcon fas fa-search"></i>
             </div>
         </div>
