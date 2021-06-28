b5214aa91d3ba03f054c6c1ae04d55afb9b43186
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-28 21:55:41 +0600

    write page design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 //import Home from "./pages/home/Home";
-import Single from "./pages/single/Single";
+//import Single from "./pages/single/Single";
 import TopBar from "./components/topbar/TopBar";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar/>
       {/* <Home /> */}
-      <Single />
+      {/* <Single /> */}
+      <Write/>
     </>
   );
 }
---
+++ src/pages/write/Write.js
@@ -0,0 +1,22 @@
+import "./write.css";
+
+export default function Write() {
+    return (
+        <div className="write">
+        <img className="writeImg" src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label>
+                        <i className="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display:"none"}} />
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea type="text" className="writeInput writeText" placeholder="Tell your story......"/>
+                </div>
+                <button className="writeSubmit">Publish</button>
+            </form>
+        </div>
+    )
+}
