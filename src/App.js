28906e6d833ab912479aae75997782c3e046f2b8
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-28 19:15:00 +0600

    single-post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Home from "./pages/home/Home";
-import TopBar from "./topbar/TopBar";
+//import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import TopBar from "./components/topbar/TopBar";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Home />
+      {/* <Home /> */}
+      <Single />
     </>
   );
 }
---
+++ src/components/SinglePost/SinglePost.js
@@ -0,0 +1,16 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import "./singlePost.css";
+
+export default function SinglePost() {
+    return (
+        <div className="singlePost">
+            <div className="singlePostWrapper">
+                <img className="" src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+                <h1 className="singlePostEdit">
+                    Lorem ipsum dolor site amet.
+                    
+                </h1>
+            </div>
+        </div>
+    )
+}
