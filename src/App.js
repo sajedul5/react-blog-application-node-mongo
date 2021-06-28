300e2eeeb9228829f43535028e3016d977101c40
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-28 23:37:54 +0600

    login & register page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Setting from "./pages/settings/Setting";
 import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       {/* <Home /> */}
       {/* <Single /> */}
       {/* <Write/> */}
-      <Setting/>
+      {/* <Setting/> */}
+      {/* <Login/> */}
+      <Register/>
     </>
   );
 }
