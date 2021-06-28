db68d202b0c59b70033b60bba23b5bef2a0e06d3
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-28 14:44:41 +0600

    header-sidebar-design

---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,36 @@
+import "./sidebar.css";
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img className="" src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt="" />
+                <p>
+                    Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+                    amet ex esse.Sunt eu ut nostrud id quis proident.  
+                </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i class="SidebarIcon fab fa-facebook"></i>
+                    <i class="SidebarIcon fab fa-twitter-square"></i>
+                    <i class="SidebarIcon fab fa-pinterest-square"></i>
+                    <i class="SidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
