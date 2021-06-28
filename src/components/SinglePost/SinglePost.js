74124cae668fdeed46e3bb241aa856253bca0d03
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-06-28 21:03:21 +0600

    singl-post-design

--- src/components/SinglePost/SinglePost.js
+++ src/components/SinglePost/SinglePost.js
@@ -5,11 +5,44 @@ export default function SinglePost() {
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img className="" src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-                <h1 className="singlePostEdit">
+                <img className="singlePostImg" src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+                <h1 className="singlePostInfo">
                     Lorem ipsum dolor site amet.
-                    
+                    <div className="singlePostedit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
                 </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Shakil</b></span>
+                    <span className="singlePostDate">1 hour age</span>
+                </div>
+                <p className="singlePostDesc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                    officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                    fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                    atque, exercitationem quibusdam, reiciendis odio laboriosam? 
+                </p>
             </div>
         </div>
     )
