68eae09ea285ec169b9d90922fc162c0ba067396
greeho951 <greeho951@gmail.com>
2021-07-09 11:58:29 +0600

    login api

--- routes/auth.js
+++ routes/auth.js
@@ -25,5 +25,23 @@ router.post("/register", async (req, res) => {
 });
 
 
+// Login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong credentials!");
+
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong credentials!");
+
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    
+    }catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
 
 module.exports = router;
