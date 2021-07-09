71bdeaef49cdcc6f6b9ecea3ac1d6c512ad90398
greeho951 <greeho951@gmail.com>
2021-07-09 12:47:55 +0600

    user delete api

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 
 
-// Register
+// Updated
 router.put("/:id", async (req, res) => {
     if(req.body.userId === req.params.id){
         
@@ -24,6 +25,31 @@ router.put("/:id", async (req, res) => {
         }
     }else{
         res.status(401).json("You can update only your account!");
+    }
+});
+
+
+
+// Delete
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId === req.params.id){
+
+       try {
+           const user = await User.findById(req.params.id);
+           try {
+            await Post.deleteMany({ username: user.username});
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted....");
+
+        }catch (err){
+            res.status(500).json(err);
+        }
+
+       }catch (err) {
+           res.status(404).json("user not found!");
+       }
+    }else{
+        res.status(401).json("You can delete only your account!");
     }
 });
 
