21fddb60d7ca332430cb5b2ed9cfad185e59ddea
greeho951 <greeho951@gmail.com>
2021-07-09 12:29:52 +0600

    user updated api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -19,6 +20,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 //Route
 app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
 
 app.use("/", (req, res) => {
     console.log("This is main url");
