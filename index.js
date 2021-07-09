d8685714f910815d801565b0341f8cfd9a384a36
greeho951 <greeho951@gmail.com>
2021-07-09 11:32:29 +0600

    register api

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 
 //  Database connection
@@ -14,6 +16,9 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(console.log("Connected to MongoDB!"))
 .catch((err) => console.log(err));
+
+//Route
+app.use("/api/auth", authRoute);
 
 app.use("/", (req, res) => {
     console.log("This is main url");
