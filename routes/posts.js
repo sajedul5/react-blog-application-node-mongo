2c07fbf067851284203dbdec2a1321bd94ac18ac
greeho951 <greeho951@gmail.com>
2021-07-11 10:47:47 +0600

    get post api

--- routes/posts.js
+++ routes/posts.js
@@ -71,13 +71,12 @@ router.delete("/:id", async (req, res) => {
 });
 
 
-// Get User
+// Get Post
 router.get("/:id", async (req, res) => {
     try {
 
-        const user = await User.findById(req.params.id);
-        const {password, ...others} = user._doc;
-        res.status(200).json(others);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
 
     }catch (err) {
         res.status(500).json(err);
